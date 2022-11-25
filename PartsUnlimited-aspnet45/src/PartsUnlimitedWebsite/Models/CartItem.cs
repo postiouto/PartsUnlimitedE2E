5d004d5e257eff5d5ea71cef92bd8adef75622ab
Posti Outo <posti.outo@gmail.com>
2022-11-25 13:22:55 +0200

    Addedd comments

--- PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
+++ PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
@@ -5,7 +5,7 @@ namespace PartsUnlimited.Models
 {
     public class CartItem : ILineItem
     {
-        // eka testi muutos
+        // toinen testi muutos
         [Key]
         public int CartItemId { get; set; }
 
--- PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/Category.cs
+++ PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/Category.cs
@@ -5,6 +5,7 @@ namespace PartsUnlimited.Models
 {
     public class Category
     {
+        // kolmas muutos
         public int CategoryId { get; set; }
 
         [Required]
